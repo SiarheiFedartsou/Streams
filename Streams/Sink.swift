48b1da76f02debd1df9bd7c9ca676a5dcfaa76f3
Siarhei Fiedartsou <Sergei_Fedortsov@cedoni.com>
2016-11-13 21:26:15 +0300

    Sink protocol implemented

--- Streams/Sink.swift
+++ Streams/Sink.swift
@@ -25,7 +25,66 @@ extension SinkProtocol {
     }
 }
 
-struct AnySink<T> {
+struct AnySink<T> : SinkProtocol {
+    
+    private let box: AnySinkBoxBase<T>
+
+    init<Base: SinkProtocol>(_ base: Base) where Base.Consumable == T
+    {
+        box = AnySinkBox(base)
+    }
+    
+    func begin(size: Int) {
+        box.begin(size: size)
+    }
+    
+    func end() {
+        box.end()
+    }
+    
+    var cancellationRequested: Bool {
+        return box.cancellationRequested
+    }
+    
+    func consume(_ t: T) {
+        box.consume(t)
+    }
+    
+}
+
+class AnySinkBoxBase<T>: SinkProtocol {
+    func begin(size: Int) {}
+    func end() {}
+    
+    var cancellationRequested: Bool { return false }
+    
+    func consume(_ t: T) {}
+}
+
+final class AnySinkBox<Base: SinkProtocol>: AnySinkBoxBase<Base.Consumable> {
+    private var base: Base
+    
+    init(_ base: Base)
+    {
+        self.base = base
+    }
+    
+    override func begin(size: Int) {
+        self.base.begin(size: size)
+    }
+    
+    override func end() {
+        self.base.end()
+    }
+    
+    override var cancellationRequested: Bool {
+        return self.base.cancellationRequested
+    }
+    
+    override func consume(_ t: Base.Consumable)
+    {
+        self.base.consume(t)
+    }
     
 }
 
