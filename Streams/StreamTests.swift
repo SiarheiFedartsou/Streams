2719f152871f819c4a7eefb5797b9d8c2d6ee3e2
Siarhei Fiedartsou <Sergei_Fedortsov@cedoni.com>
2016-11-14 19:43:15 +0300

    Test for empty stream case added

--- Streams/StreamTests.swift
+++ Streams/StreamTests.swift
@@ -12,6 +12,19 @@ import Nimble
 
 class StreamTests: XCTestCase {
     
+    func testThatForEachWorksForEmptyStream() {
+        // given
+        let array = [Int]()
+        
+        // when
+        var result = [Int]()
+        array.stream
+            .forEach({ result.append($0) })
+        
+        // then
+        expect(result).to(equal([]))
+    }
+    
     func testThatForEachWorksForJustCreatedStream() {
         // given
         let array = [12, 4, 9, 14, 10, 3]
