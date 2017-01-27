1685e3d4a6e6b3e74a74967b1a56d512d15ab908
Siarhei Fiedartsou <Sergei_Fedortsov@cedoni.com>
2017-01-27 15:13:12 +0300

    CollectionNode added

---
+++ StreamsEx/StreamsEx/Node/CollectionNode.swift
@@ -0,0 +1,32 @@
+//
+//  CollectionNode.swift
+//  StreamsEx
+//
+//  Created by Sergey Fedortsov on 27.01.17.
+//  Copyright © 2017 Sergey Fedortsov. All rights reserved.
+//
+
+struct CollectionNode<T> : Node {
+    // TODO: must be AnyCollection in the future
+    private let collection: AnyRandomAccessCollection<T>
+    
+    var spliterator: AnySpliterator<T> {
+        return collection.spliterator
+    }
+    
+    func forEach(_ each: (T) -> ()) {
+        collection.forEach(each)
+    }
+    
+    func array() -> [T] {
+        var array = [T]()
+        for element in collection {
+            array.append(element)
+        }
+        return array
+    }
+    
+    var count: IntMax {
+        return collection.count
+    }
+}
