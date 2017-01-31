5cc8143df0ab61a58f26475fa091ff1580550170
Siarhei Fedartsou <siarhei.fedartsou@gmail.com>
2017-01-31 21:32:22 +0300

    `distinct` benchmarks added

---
+++ StreamsEx/StreamsEx/Distinct/DistinctBenchmarks.swift
@@ -0,0 +1,52 @@
+//
+//  DistinctBenchmarks.swift
+//  StreamsEx
+//
+//  Created by Siarhei Fedartsou on 31.01.17.
+//  Copyright © 2017 Sergey Fedortsov. All rights reserved.
+//
+
+import XCTest
+@testable import StreamsEx
+import Nimble
+
+class DistinctBenchmarks: XCTestCase {
+
+    
+    func testSequential() {
+        var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        for _ in 1..<12 {
+            array.append(contentsOf: array)
+        }
+        
+        var sum: Int = 0
+        self.measure {
+            sum = array.stream.map {
+                usleep(1)
+                return $0
+            }.distinct().reduce(identity: 0, accumulator: +)
+        }
+        
+        expect(sum).to(equal(55))
+    }
+    
+    
+    func testParallel() {
+        var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        for _ in 1..<12 {
+            array.append(contentsOf: array)
+        }
+        
+        var sum: Int = 0
+        self.measure {
+            sum = array.parallelStream.map {
+                usleep(1)
+                return $0
+            }.distinct().reduce(identity: 0, accumulator: +)
+        }
+        
+        expect(sum).to(equal(55))
+    }
+    
+    
+}
